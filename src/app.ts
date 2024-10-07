54fb9f502336fd68614427f9480d3ab725006738
StoyanDimitrov0016 <stoyan1020@gmail.com>
2024-10-07 23:55:18 +0300

    feat: Integrate Clerk to the project

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,24 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
+import { clerkMiddleware, getAuth } from "@clerk/express";
+import articleCore from "./routes/articleCore.router.js";
+
+const secretKey = process.env.CLERK_SECRET_KEY;
+const publishableKey = process.env.CLERK_PUBLISHABLE_KEY;
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  clerkMiddleware({
+    secretKey,
+    publishableKey,
+  })
+);
+
+app.use("/api/v1/articles/", articleCore);
 
 export default app;
