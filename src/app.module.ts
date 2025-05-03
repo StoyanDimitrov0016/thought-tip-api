1660f1a5bbf0684e4e023fe72b3df11050e16219
StoyanDimitrov0016 <stoyan1020@gmail.com>
2025-05-03 13:17:12 +0300

    feat: add config module as an import in the app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SegmentationModule } from './segmentation/segmentation.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [SegmentationModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+    SegmentationModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
