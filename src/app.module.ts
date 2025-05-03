8e10822ea12424d8bfbc242093030f72a0840e73
StoyanDimitrov0016 <stoyan1020@gmail.com>
2025-05-03 14:30:02 +0300

    feat: add prisma global module and append it to the app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SegmentationModule } from './segmentation/segmentation.module';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
     SegmentationModule,
+    PrismaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
