8d95c10a7f44a6232a7258fa1725a4ad4543e39f
StoyanDimitrov0016 <stoyan1020@gmail.com>
2025-05-10 14:28:20 +0300

    refactor: set every route to be in a plural form

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { ArticleService } from './article.service';
 import { CreateArticleDto } from './dto/create-article.dto';
 import { UpdateArticleDto } from './dto/update-article.dto';
 
-@Controller('article')
+@Controller('articles')
 export class ArticleController {
   constructor(private readonly articleService: ArticleService) {}
 
